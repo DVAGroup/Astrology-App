51c49c6e494a06dcb70bfb0a5244333e35fa3b8e
Yeprem <yeprem.ayvazyan@rorotika.com>
2019-10-03 18:58:52 +0200

    adding latest changes without paypal

--- app/src/main/java/com/example/realtimeastrology/MainActivity3.java
+++ app/src/main/java/com/example/realtimeastrology/MainActivity3.java
@@ -2,7 +2,9 @@ package com.example.realtimeastrology;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+
+    public void SendEmail(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"Harry.waldauer@gmail.com"});
+        intent.putExtra(Intent.EXTRA_CC, new String[] {"yeprem.ayvazyan@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Mail from Real Time Astrology!");
+        intent.putExtra(Intent.EXTRA_TEXT, "Testing email function please confirm with Yeprem");
+
+        try {
+            startActivity(Intent.createChooser(intent, "Testing email?"));
+        } catch (android.content.ActivityNotFoundException ex) {
+            //do something else
+        }
     }
 }
